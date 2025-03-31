15e4c9362ea64eaa0be3c8e9e7981c41026c5550
Jacob Hobbie <hey@jacobhobbie.com>
2025-03-31 02:28:10 +0000

    Correcting 2nd Fastmail MX Record

--- dnsconfig.js
+++ dnsconfig.js
@@ -11,7 +11,7 @@ D("jacobhobbie.com", REG_NONE,
     CNAME("fm2._domainkey", "fm2.jacobhobbie.com.dkim.fmhosted.com."),
     CNAME("fm3._domainkey", "fm3.jacobhobbie.com.dkim.fmhosted.com."),
     MX("@", 10, "in1-smtp.messagingengine.com."),
-    MX("@", 20, "in1-smtp.messagingengine.com."),
+    MX("@", 20, "in2-smtp.messagingengine.com."),
     TXT("@", "v=spf1 include:spf.messagingengine.com ?all"),
     TXT("pronouns", "he/him")
 );
