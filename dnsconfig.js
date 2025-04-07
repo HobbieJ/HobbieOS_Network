03e47ab429f45beba0567ae3ece1012f11057742
Jacob Hobbie <hey@jacobhobbie.com>
2025-04-06 23:46:20 -0500

    Update dnsconfig.js

--- dnsconfig.js
+++ dnsconfig.js
@@ -20,9 +20,7 @@ D("jacobhobbie.com", REG_PORKBUN,
     MX("@", 20, "in2-smtp.messagingengine.com."),
     TXT("@", "\"v=spf1 include:spf.messagingengine.com ?all\""),
     // Bluesky Settings
-    TXT("_atproto", "\"did=did:plc:ctqbmzjkhqkhpftpm6ajt5hx\""),
-    // Extras
-    TXT("pronouns", "\"he/him\"")
+    TXT("_atproto", "\"did=did:plc:ctqbmzjkhqkhpftpm6ajt5hx\"")
 );
 
 D("hobbie.dev", REG_PORKBUN,
